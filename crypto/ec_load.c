4cc2e7c0a273d886ca50ea0992af14fe14a1d60a
Songa7273 <k.gaju@alustudent.com>
2026-09-02 09:14:26 +0000

    crypto

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -4,6 +4,27 @@
 #include <stdio.h>
 
 /**
+ * ec_load_pub - Loads the public key from disk
+ * @path: Path to the public key file
+ *
+ * Return: Pointer to EC_KEY, or NULL upon failure
+ */
+static EC_KEY *ec_load_pub(char const *path)
+{
+	FILE *file;
+	EC_KEY *key;
+
+	file = fopen(path, "r");
+	if (file == NULL)
+		return (NULL);
+
+	key = PEM_read_EC_PUBKEY(file, NULL, NULL, NULL);
+	fclose(file);
+
+	return (key);
+}
+
+/**
  * ec_load - Loads an EC key pair from disk
  * @folder: Folder containing the key files
  *
@@ -14,6 +35,7 @@ EC_KEY *ec_load(char const *folder)
 	FILE *file;
 	char path[4096];
 	EC_KEY *key;
+	EC_KEY *pubkey;
 
 	if (folder == NULL)
 		return (NULL);
@@ -29,20 +51,20 @@ EC_KEY *ec_load(char const *folder)
 		return (NULL);
 
 	snprintf(path, sizeof(path), "%s/%s", folder, PUB_FILENAME);
-	file = fopen(path, "r");
-	if (file == NULL)
+	pubkey = ec_load_pub(path);
+	if (pubkey == NULL)
 	{
 		EC_KEY_free(key);
 		return (NULL);
 	}
 
-	if (PEM_read_EC_PUBKEY(file, &key, NULL, NULL) == NULL)
+	if (EC_KEY_set_public_key(key, EC_KEY_get0_public_key(pubkey)) != 1)
 	{
-		fclose(file);
+		EC_KEY_free(pubkey);
 		EC_KEY_free(key);
 		return (NULL);
 	}
 
-	fclose(file);
+	EC_KEY_free(pubkey);
 	return (key);
 }
