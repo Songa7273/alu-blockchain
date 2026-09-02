d732e076629e5995205bfa2d85490c4daadd7ad1
Songa7273 <k.gaju@alustudent.com>
2026-09-02 10:29:29 +0000

    Fix crypto Betty style

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -9,25 +9,25 @@
  */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-EC_GROUP const *group;
-EC_POINT const *point;
-size_t len;
+	EC_GROUP const *group;
+	EC_POINT const *point;
+	size_t len;
 
-if (key == NULL || pub == NULL)
-return (NULL);
+	if (key == NULL || pub == NULL)
+		return (NULL);
 
-group = EC_KEY_get0_group(key);
-point = EC_KEY_get0_public_key(key);
+	group = EC_KEY_get0_group(key);
+	point = EC_KEY_get0_public_key(key);
 
-if (group == NULL || point == NULL)
-return (NULL);
+	if (group == NULL || point == NULL)
+		return (NULL);
 
-len = EC_POINT_point2oct(group, point,
- POINT_CONVERSION_UNCOMPRESSED,
- pub, EC_PUB_LEN, NULL);
+	len = EC_POINT_point2oct(group, point,
+				 POINT_CONVERSION_UNCOMPRESSED,
+				 pub, EC_PUB_LEN, NULL);
 
-if (len != EC_PUB_LEN)
-return (NULL);
+	if (len != EC_PUB_LEN)
+		return (NULL);
 
-return (pub);
+	return (pub);
 }
