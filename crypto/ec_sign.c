c3d4743f78d90468b58a129796bf6d390ad3ea6b
Songa7273 <k.gaju@alustudent.com>
2026-09-02 09:43:20 +0000

    crypto

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -16,21 +16,17 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 		size_t msglen, sig_t *sig)
 {
-	uint8_t digest[SHA256_DIGEST_LENGTH];
 	unsigned int siglen;
 
 	if (key == NULL || msg == NULL || sig == NULL)
-		return (NULL);
-
-	if (SHA256(msg, msglen, digest) == NULL)
 		return (NULL);
 
 	sig->sig = malloc(ECDSA_size(key));
 	if (sig->sig == NULL)
 		return (NULL);
 
-	if (ECDSA_sign(0, digest, SHA256_DIGEST_LENGTH,
-		       sig->sig, &siglen, (EC_KEY *)key) != 1)
+	if (ECDSA_sign(0, msg, msglen, sig->sig, &siglen,
+		       (EC_KEY *)key) != 1)
 	{
 		free(sig->sig);
 		sig->sig = NULL;
