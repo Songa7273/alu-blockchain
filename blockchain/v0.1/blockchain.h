ee5a2f4a0a2a5ba26d7bf75d2f05330c6095075c
Songa7273 <k.gaju@alustudent.com>
2026-09-09 06:55:49 +0000

    Fix missing llist.h include in blockchain.h and implement blockchain_create

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -1,56 +1,77 @@
 #ifndef BLOCKCHAIN_H
 #define BLOCKCHAIN_H
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <llist.h>
 
 #define SHA256_DIGEST_LENGTH 32
-#define BLOCKCHAIN_DATA_MAX 1024
+#define BLOCKCHAIN_DATA_MAX_LEN 1024
+
+#define GENESIS_INDEX 0
+#define GENESIS_DIFFICULTY 0
+#define GENESIS_TIMESTAMP 1537578000
+#define GENESIS_NONCE 0
+#define GENESIS_PREV_HASH "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+#define GENESIS_DATA "Holberton School"
+#define GENESIS_DATA_LEN 16
+#define GENESIS_HASH "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"
+
+/**
+ * struct block_info_s - Block info structure
+ *
+ * @index:      Index of the Block in the Blockchain
+ * @difficulty: Difficulty of the Proof-of-Work
+ * @timestamp:  Time at which the Block was created
+ * @nonce:      Salt used to alter the hash
+ * @prev_hash:  Hash the previous Block in the Blockchain
+ */
+typedef struct block_info_s
+{
+	uint32_t index;
+	uint32_t difficulty;
+	uint64_t timestamp;
+	uint64_t nonce;
+	uint8_t prev_hash[SHA256_DIGEST_LENGTH];
+} block_info_t;
+
+/**
+ * struct block_data_s - Block data structure
+ *
+ * @buffer: Data buffer
+ * @len:    Data length
+ */
+typedef struct block_data_s
+{
+	int8_t buffer[BLOCKCHAIN_DATA_MAX_LEN];
+	uint32_t len;
+} block_data_t;
 
 /**
  * struct block_s - Block structure
+ *
  * @info: Block information
  * @data: Block data
  * @hash: Block hash
  */
 typedef struct block_s
 {
-	struct
-	{
-		uint32_t index;
-		uint32_t difficulty;
-		uint64_t timestamp;
-		uint64_t nonce;
-		uint8_t prev_hash[SHA256_DIGEST_LENGTH];
-	} info;
-
-	struct
-	{
-		int8_t *buffer;
-		uint32_t len;
-	} data;
-
+	block_info_t info;
+	block_data_t data;
 	uint8_t hash[SHA256_DIGEST_LENGTH];
 } block_t;
 
 /**
  * struct blockchain_s - Blockchain structure
- * @chain: Linked list containing the blocks
+ *
+ * @chain: Pointer to the head of the linked list containing blocks
  */
 typedef struct blockchain_s
 {
 	llist_t *chain;
 } blockchain_t;
 
+/* Function prototypes */
 blockchain_t *blockchain_create(void);
-block_t *block_create(block_t const *prev, int8_t const *data,
-	uint32_t data_len);
-void block_destroy(block_t *block);
-void blockchain_destroy(blockchain_t *blockchain);
-uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
-int blockchain_serialize(blockchain_t const *blockchain, char const *path);
-blockchain_t *blockchain_deserialize(char const *path);
-int block_is_valid(block_t const *block, block_t const *prev_block);
 
 #endif /* BLOCKCHAIN_H */
--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -1,80 +1,71 @@
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
-
 #include "blockchain.h"
 
 /**
- * genesis_init - Initialize the Genesis block
- * @genesis: Genesis block to initialize
+ * create_genesis_block - Helper function to allocate and initialize
+ *                        the static Genesis block
+ *
+ * Return: Pointer to the created genesis block, or NULL on failure
  */
-static void genesis_init(block_t *genesis)
+static block_t *create_genesis_block(void)
 {
-	genesis->info.index = 0;
-	genesis->info.difficulty = 0;
-	genesis->info.timestamp = 1537578000;
-	genesis->info.nonce = 0;
+	block_t *genesis = calloc(1, sizeof(block_t));
 
+	if (!genesis)
+		return (NULL);
+
+	genesis->info.index = GENESIS_INDEX;
+	genesis->info.difficulty = GENESIS_DIFFICULTY;
+	genesis->info.timestamp = GENESIS_TIMESTAMP;
+	genesis->info.nonce = GENESIS_NONCE;
 	memset(genesis->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
 
-	genesis->data.buffer = malloc(16);
-	if (genesis->data.buffer != NULL)
-	{
-		memcpy(genesis->data.buffer, "Holberton School", 16);
-		genesis->data.len = 16;
-	}
-	else
-	{
-		genesis->data.len = 0;
-	}
+	memcpy(genesis->data.buffer, GENESIS_DATA, GENESIS_DATA_LEN);
+	genesis->data.len = GENESIS_DATA_LEN;
 
-	memcpy(genesis->hash,
-	       "\xc5\x2c\x26\xc8\xb5\x46\x16\x39"
-	       "\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d"
-	       "\x0c\x8e\x00\x09\xc8\x17\xf2\xb1"
-	       "\xd3\xd7\xff\x2f\x04\x51\x58\x03",
-	       SHA256_DIGEST_LENGTH);
+	memcpy(genesis->hash, GENESIS_HASH, SHA256_DIGEST_LENGTH);
+
+	return (genesis);
 }
 
 /**
- * blockchain_create - Create and initialize a blockchain
+ * blockchain_create - Creates a new Blockchain structure and initializes it
+ *                     with the Genesis block
  *
- * Return: Pointer to the new blockchain, or NULL on failure
+ * Return: Pointer to the created blockchain_t structure, or NULL on failure
  */
 blockchain_t *blockchain_create(void)
 {
-	blockchain_t *blockchain;
-	block_t *genesis;
+	blockchain_t *blockchain = NULL;
+	block_t *genesis = NULL;
 
-	blockchain = malloc(sizeof(*blockchain));
-	if (blockchain == NULL)
+	blockchain = malloc(sizeof(blockchain_t));
+	if (!blockchain)
 		return (NULL);
 
 	blockchain->chain = llist_create();
-	if (blockchain->chain == NULL)
+	if (!blockchain->chain)
 	{
 		free(blockchain);
 		return (NULL);
 	}
 
-	genesis = malloc(sizeof(*genesis));
-	if (genesis == NULL)
+	genesis = create_genesis_block();
+	if (!genesis)
 	{
-		llist_destroy(blockchain->chain, 0, NULL);
+		llist_destroy(blockchain->chain, 1, NULL);
 		free(blockchain);
 		return (NULL);
 	}
 
-	genesis_init(genesis);
-
-	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_BACK) != 0)
+	if (llist_add_node(blockchain->chain, genesis, ADD_NODE_FRONT) != 0)
 	{
 		free(genesis);
-		llist_destroy(blockchain->chain, 0, NULL);
+		llist_destroy(blockchain->chain, 1, NULL);
 		free(blockchain);
 		return (NULL);
 	}
 
 	return (blockchain);
 }
-
