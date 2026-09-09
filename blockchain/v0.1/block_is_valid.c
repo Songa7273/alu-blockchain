0ae667cff13356b9f9f31030bd2c8a0911710e71
Songa7273 <k.gaju@alustudent.com>
2026-09-09 08:29:27 +0000

    introduction to blockchain

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -1,68 +1,48 @@
-#include <string.h>
 #include "blockchain.h"
 
-/**
- * check_genesis - Checks if a given block matches the static Genesis block
- * @block: Pointer to the block to check
- *
- * Return: 0 if block is valid Genesis block, 1 otherwise
- */
-static int check_genesis(block_t const *block)
-{
-	block_t genesis;
-
-	if (!block || block->info.index != 0)
-		return (1);
-
-	memset(&genesis, 0, sizeof(genesis));
-	genesis.info.index = 0;
-	genesis.info.difficulty = 0;
-	genesis.info.timestamp = 1537578000;
-	genesis.info.nonce = 0;
-	memset(genesis.info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-
-	memcpy(genesis.data.buffer, "Holberton School", 16);
-	genesis.data.len = 16;
-
-	memcpy(genesis.hash,
-	       "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d"
-	       "\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03",
-	       SHA256_DIGEST_LENGTH);
-
-	return (memcmp(block, &genesis, sizeof(genesis)) != 0);
-}
+/* GENESIS_BLOCK - first block in the blockchain */
+#define GENESIS_BLOCK                                                          \
+	{                                                                      \
+	    {                                                                  \
+		0 /* index */,                                                 \
+		0,	    /* difficulty */                                   \
+		1537578000, /* timestamp */                                    \
+		0,	    /* nonce */                                        \
+		{0}	    /* prev_hash */                                    \
+	    },                                                                 \
+	    {                                                                  \
+		"Holberton School", /* buffer */                               \
+		16		    /* len */                                  \
+	    },			    /* hashed data */                          \
+	    "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
+	    "\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03"}
 
 /**
- * block_is_valid - Verifies that a Block is valid
- * @block: Pointer to the Block to check
- * @prev_block: Pointer to the previous Block in the Blockchain
- *
- * Return: 0 if valid, 1 otherwise
+ * block_is_valid - checks if this and previous block are valid
+ * @block: pointer to this block in the chain
+ * @prev_block: pointer to previous block in the chain or NULL
+ * Return: 0 if valid else 1 if invalid
  */
 int block_is_valid(block_t const *block, block_t const *prev_block)
 {
-	uint8_t hash[SHA256_DIGEST_LENGTH];
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH] = {0};
+	block_t const _genesis = GENESIS_BLOCK;
 
-	if (!block || block->data.len > BLOCKCHAIN_DATA_MAX_LEN)
+	if (!block || (!prev_block && block->info.index != 0))
 		return (1);
-
-	if (!prev_block)
-		return (check_genesis(block));
-
+	if (block->info.index == 0)
+		return (memcmp(block, &_genesis, sizeof(_genesis)));
 	if (block->info.index != prev_block->info.index + 1)
 		return (1);
-
-	if (!block_hash(prev_block, hash) ||
-	    memcmp(prev_block->hash, hash, SHA256_DIGEST_LENGTH) != 0)
+	if (!block_hash(prev_block, hash_buf) ||
+	    memcmp(hash_buf, prev_block->hash, SHA256_DIGEST_LENGTH))
 		return (1);
-
-	if (memcmp(block->info.prev_hash, prev_block->hash,
-		   SHA256_DIGEST_LENGTH) != 0)
+	if (memcmp(prev_block->hash, block->info.prev_hash, SHA256_DIGEST_LENGTH))
 		return (1);
-
-	if (!block_hash(block, hash) ||
-	    memcmp(block->hash, hash, SHA256_DIGEST_LENGTH) != 0)
+	if (!block_hash(block, hash_buf) ||
+	    memcmp(hash_buf, block->hash, SHA256_DIGEST_LENGTH))
 		return (1);
-
+	if (block->data.len > BLOCKCHAIN_DATA_MAX)
+		return (1);
 	return (0);
 }
